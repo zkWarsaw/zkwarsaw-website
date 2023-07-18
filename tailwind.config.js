1b36b7d04aaa683aa4332779111d417e2796c9b8
Orest Tarasiuk <830847+OrestTa@users.noreply.github.com>
2023-07-18 18:36:35 +0200

    style: set and run formatter (#8)
    * prettier format
    
    * prettier-ignore autogenerated tailwind css
    
    * run prettier on codebase
    
    ---------
    
    Co-authored-by: igortrsk <igor.tarasiuk4@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
-  theme: {
-    extend: {
-      colors: {
-        brand: {
-          'green': '#1BFF68',
-          'blue': '#0029D8',
-          'sky-blue': '#06ABF2',
-          'gray': '#D9D9D9',
-        }
-      },
-      fontFamily: {
-        'oxanium': ['Oxanium', 'cursive'],
-      },
-      backgroundImage: {
-        'hero-bg': "url('../img/hero.png')",
-        "waves-bg": "url('../img/layered-waves-haikei.svg')",
-        "waves-bg-upsd": "url('../img/layered-waves-haikei-upsd.svg')",
-      },
-      grayscale: {
-        80: "80%",
-        40: "40%",
-        20: "20%",
-      },
+    content: ['./public/**/*.{html,js}'],
+    theme: {
+        extend: {
+            colors: {
+                brand: {
+                    green: '#1BFF68',
+                    blue: '#0029D8',
+                    'sky-blue': '#06ABF2',
+                    gray: '#D9D9D9',
+                },
+            },
+            fontFamily: {
+                oxanium: ['Oxanium', 'cursive'],
+            },
+            backgroundImage: {
+                'hero-bg': "url('../img/hero.png')",
+                'waves-bg': "url('../img/layered-waves-haikei.svg')",
+                'waves-bg-upsd': "url('../img/layered-waves-haikei-upsd.svg')",
+                'waves-bg-meetings': "url('../img/layered-waves-meetings.svg')",
+            },
+            grayscale: {
+                80: '80%',
+                40: '40%',
+                20: '20%',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
